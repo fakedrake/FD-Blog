d3b24c271980dc86cb24768cf3f7d1ca8709af8b
Chris Perivolaropoulos <darksaga2006@gmail.com>
2013-01-23 06:34:28 +0200

    Deployment details

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [com.novemberain/monger "1.4.2"]]
+            :dev-dependencies [[lein-ring "0.7.3"]]
+            :ring {:handler fd-blog.server/handler}
             :main fd-blog.server)
--- src/fd_blog/server.clj
+++ src/fd_blog/server.clj
@@ -1,10 +1,29 @@
 (ns fd-blog.server
-  (:require [noir.server :as server]))
+  (:require [noir.server :as server]
+            [fd-blog.views.post])
+  (:gen-class))
 
 (server/load-views-ns 'fd-blog.views)
+
+(def base-handler
+  (server/gen-handler
+    {:mode :prod,
+     :ns 'fd-blog
+     :session-cookie-attrs {:max-age 1800000}}))
 
 (defn -main [& m]
   (let [mode (keyword (or (first m) :dev))
         port (Integer. (get (System/getenv) "PORT" "8080"))]
     (server/start port {:mode mode
                         :ns 'fd-blog})))
+
+(defn fix-base-url [handler]
+  (fn [request]
+    (with-redefs [noir.options/resolve-url
+                  (fn [url]
+                    ;prepend context to the relative URLs
+                    (if (.contains url "://")
+                      url (str (:context request) url)))]
+      (handler request))))
+
+(def handler (-> base-handler fix-base-url))
